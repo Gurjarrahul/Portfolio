d1edfaabe93cec81cbefec68d4fedc0a860ecf4e
Gurjarrahul <rahulgur08@gmail.com>
2023-10-28 00:45:22 +0530

    Initial commit

---
+++ src/components/Contact/index.js
@@ -0,0 +1,201 @@
+import React,{useState} from 'react'
+import styled from 'styled-components'
+import { Snackbar } from '@mui/material';
+import {contact} from '../../data/constants'
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+position: relative;
+z-index: 1;
+align-items: center;
+@media (max-width: 960px) {
+    padding: 0px;
+}
+`
+
+const Wrapper = styled.div`
+position: relative;
+display: flex;
+justify-content: space-between;
+align-items: center;
+flex-direction: column;
+width: 100%;
+max-width: 1350px;
+padding: 0px 0px 80px 0px;
+gap: 12px;
+@media (max-width: 960px) {
+    flex-direction: column;
+}
+`
+
+const Title = styled.div`
+font-size: 42px;
+text-align: center;
+font-weight: 600;
+margin-top: 20px;
+  color: ${({ theme }) => theme.text_primary};
+  @media (max-width: 768px) {
+      margin-top: 12px;
+      font-size: 32px;
+  }
+`;
+
+const Desc = styled.div`
+    font-size: 18px;
+    text-align: center;
+    max-width: 600px;
+    color: ${({ theme }) => theme.text_secondary};
+    @media (max-width: 768px) {
+        margin-top: 12px;
+        font-size: 16px;
+    }
+`;
+
+
+const ContactForm = styled.form`
+  width: 95%;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.card};
+  padding: 32px;
+  border-radius: 16px;
+  box-shadow: rgba(23, 92, 230, 0.15) 0px 4px 24px;
+  margin-top: 28px;
+  gap: 12px;
+`
+const Image = styled.img`
+  width: 24px;
+  height: 24px;
+  margin-right:20px;
+`
+const ContactTitle = styled.div`
+  font-size: 24px;
+  margin-bottom: 6px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.text_primary};
+`
+
+const ContactInput = styled.input`
+  flex: 1;
+  background-color: transparent;
+  border: 1px solid ${({ theme }) => theme.text_secondary};
+  outline: none;
+  font-size: 18px;
+  color: ${({ theme }) => theme.text_primary};
+  border-radius: 12px;
+  padding: 12px 16px;
+  &:focus {
+    border: 1px solid ${({ theme }) => theme.primary};
+  }
+`
+
+const ContactInputMessage = styled.textarea`
+  flex: 1;
+  background-color: transparent;
+  border: 1px solid ${({ theme }) => theme.text_secondary};
+  outline: none;
+  font-size: 18px;
+  color: ${({ theme }) => theme.text_primary};
+  border-radius: 12px;
+  padding: 12px 16px;
+  &:focus {
+    border: 1px solid ${({ theme }) => theme.primary};
+  }
+`
+
+const ContactButton = styled.input`
+  width: 100%;
+  text-decoration: none;
+  text-align: center;
+  background: hsla(271, 100%, 50%, 1);
+  background: linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
+  background: -moz-linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
+  background: -webkit-linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
+  padding: 13px 16px;
+  margin-top: 2px;
+  border-radius: 12px;
+  border: none;
+  color: ${({ theme }) => theme.text_primary};
+  font-size: 18px;
+  font-weight: 600;
+`
+
+
+
+const Contact = () => {
+  const formInitialDetails = {
+    Name: '',
+    email: '',
+    phone: '',
+    message: ''
+  }
+  const [formDetails, setFormDetails] = useState(formInitialDetails);
+  const [buttonText, setButtonText] = useState('Send');
+  const [open, setOpen] = useState(false);
+
+  const onFormUpdate = (category, value) => {
+    setFormDetails({
+      ...formDetails,
+      [category]: value
+    })
+}
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      setButtonText("Sending...");
+      let response = await fetch("http://localhost:5000/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify(formDetails),
+      });
+      setButtonText("Send");
+      let result = await response.json();
+      setFormDetails(formInitialDetails);
+      if (result.code == 200) {
+        setOpen(true);
+      } else {
+        setOpen(false);
+      }
+    };
+
+
+
+  return (
+    <Container>
+      <Wrapper>
+        <Title>Contact</Title>
+        <Desc>Feel free to reach out to me for any questions or opportunities!</Desc>
+        <ContactForm >
+          <ContactTitle>Email Me 🚀</ContactTitle>
+          <ContactInput type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)}  />
+          <ContactInput type="text" value={formDetails.Name} placeholder="Your Name" onChange={(e) => onFormUpdate('Name', e.target.value)} />
+          <ContactInput type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)} />
+          <ContactInputMessage placeholder="Message" rows="4" onChange={(e) => onFormUpdate('message', e.target.value)} />
+          <ContactButton type="submit" onSubmit={handleSubmit} />
+        </ContactForm>
+        
+        <ContactForm >
+          <ContactTitle>Location:</ContactTitle>
+          <p style={{'color':'white'}}><Image src={contact.locimg}/>{contact.locdet}</p>
+          <ContactTitle>Email:</ContactTitle>
+          <p style={{'color':'white'}}><Image src={contact.mailimg}/><a href={`mailto:${contact.mail}`} >{contact.mail}</a></p>
+          <ContactTitle>Moblie:</ContactTitle>
+          <p style={{'color':'white'}}><Image src={contact.phimg}/><a href={`tel:${contact.phno}`}>{contact.phno}</a></p>
+        </ContactForm>
+        <Snackbar
+          open={open}
+          autoHideDuration={6000}
+          onClose={()=>setOpen(false)}
+          message="Email sent successfully!"
+          severity="success"
+        />
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Contact
